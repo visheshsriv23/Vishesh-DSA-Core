fab402187ad3b0ea1b0debcf6ce20e5847d86de9
Mac <mac@Vishesh-Device.local>
2026-06-19 11:25:17 +0530

    Initial Setup

---
+++ LinkedList/LL_Cycle.java
@@ -0,0 +1,34 @@
+public class LL_Cycle {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true; // Cycle detected
+            }
+        }
+        return false; // No cycle
+    }
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head; // Creating a cycle
+        System.out.println(isCycle()); // Output: true
+    }
+}
